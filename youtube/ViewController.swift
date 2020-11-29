546547123c69554856c9ed5447e18d31ff43c64a
keisukegocho <gomaro0507@gmail.com>
2020-11-29 17:52:54 +0900

    .gitignoreを反映

---
+++ youtube/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  youtube
+//
+//  Created by Keisuke Gocho on 2020/11/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
